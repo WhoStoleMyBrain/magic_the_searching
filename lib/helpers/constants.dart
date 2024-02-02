4431745b708f984058d51b7c06bd6fcb677ab8c5
WhoStoleMyBrain <marcowetter@gmx.de>
2024-02-02 07:34:17 +0100

    Added: constants for context in routing process between search screen and input mask

--- lib/helpers/constants.dart
+++ lib/helpers/constants.dart
@@ -3,4 +3,15 @@ class Constants {
       'https://api.scryfall.com/catalog/keyword-abilities';
   static const urlCreatureTypes =
       'https://api.scryfall.com/catalog/creature-types';
+  static const urlSets = 'https://api.scryfall.com/sets';
+  static const contextSearchTerm = 'searchTerm';
+  static const contextLanguages = 'languages';
+  static const contextCreatureTypes = 'selectedCreatureTypes';
+  static const contextCardTypes = 'selectedCardTypes';
+  static const contextKeywords = 'selectedKeywordAbilities';
+  static const contextSet = 'set';
+  static const contextCmcValue = 'cmcValue';
+  static const contextCmcCondition = 'cmcCondition';
+  static const contextColors = 'colors';
+  static const contextManaSymbols = 'colors';
 }
