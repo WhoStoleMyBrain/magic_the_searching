8144aebd9a8525528e37cc75e2884d0e1b9f2a8c
WhoStoleMyBrain <marcowetter@gmx.de>
2024-02-20 19:42:38 +0100

    Added: various table creation sql statements  and other constants

--- lib/helpers/constants.dart
+++ lib/helpers/constants.dart
@@ -34,6 +34,37 @@ class Constants {
   static const settingUserLanguage = 'userLanguage';
   static const settingUseImagesFromNet = 'useImagesFromNet';
   static const settingDbUpdatedAt = 'dbUpdatedAt';
+  static const settingUseLocalDB = 'useLocalDB';
+  static const settingIsFirstLoaded = 'isFirstLoaded';
   static const placeholderSplitText = 'PLACEHOLDER_SPLIT_TEXT';
   static const loyaltyAssetPath = 'assets/images/Loyalty.svg';
+  static String defaultTimestamp =
+      DateTime.parse("1969-07-20 20:18:04Z").toIso8601String();
+
+  static const createCardInfoTable =
+      'CREATE TABLE card_info(id TEXT UNIQUE PRIMARY KEY, oracleId TEXT, scryfallUri TEXT, dateTime DATETIME);';
+  static const createCardDetailTable =
+      'CREATE TABLE card_detail(id TEXT UNIQUE PRIMARY KEY, name TEXT, printedName TEXT, manaCost TEXT, typeLine TEXT, printedTypeLine TEXT, oracleText TEXT, printedText TEXT, power TEXT, toughness TEXT, loyalty TEXT, setName TEXT, flavorText TEXT, hasTwoSides INTEGER, FOREIGN KEY(id) REFERENCES card_info(id));';
+  static const createImageUrisTable =
+      'CREATE TABLE image_uris(id TEXT UNIQUE PRIMARY KEY, normal TEXT, small TEXT, FOREIGN KEY(id) REFERENCES card_info(id));';
+  static const createCardFacesTable =
+      'CREATE TABLE card_faces(id TEXT UNIQUE PRIMARY KEY, normalFront TEXT, smallFront TEXT, normalBack TEXT, smallBack TEXT, FOREIGN KEY(id) REFERENCES card_info(id));';
+  static const createPricesTable =
+      'CREATE TABLE prices(id TEXT UNIQUE PRIMARY KEY, usd TEXT, usdFoil TEXT, eur TEXT, eurFoil TEXT, FOREIGN KEY(id) REFERENCES card_info(id));';
+  static const createPurchaseUrisTable =
+      'CREATE TABLE purchase_uris(id TEXT UNIQUE PRIMARY KEY, tcgplayer TEXT, cardmarket TEXT, FOREIGN KEY(id) REFERENCES card_info(id));';
+
+  static const createCardInfoTableIfNotExists =
+      'CREATE TABLE IF NOT EXISTS card_info(id TEXT UNIQUE PRIMARY KEY, oracleId TEXT, scryfallUri TEXT, dateTime DATETIME);';
+  static const createCardDetailTableIfNotExists =
+      'CREATE TABLE IF NOT EXISTS card_detail(id TEXT UNIQUE PRIMARY KEY, name TEXT, printedName TEXT, manaCost TEXT, typeLine TEXT, printedTypeLine TEXT, oracleText TEXT, printedText TEXT, power TEXT, toughness TEXT, loyalty TEXT, setName TEXT, flavorText TEXT, hasTwoSides INTEGER, FOREIGN KEY(id) REFERENCES card_info(id));';
+  static const createImageUrisTableIfNotExists =
+      'CREATE TABLE IF NOT EXISTS image_uris(id TEXT UNIQUE PRIMARY KEY, normal TEXT, small TEXT, FOREIGN KEY(id) REFERENCES card_info(id));';
+  static const createCardFacesTableIfNotExists =
+      'CREATE TABLE IF NOT EXISTS card_faces(id TEXT UNIQUE PRIMARY KEY, normalFront TEXT, smallFront TEXT, normalBack TEXT, smallBack TEXT, FOREIGN KEY(id) REFERENCES card_info(id));';
+  static const createPricesTableIfNotExists =
+      'CREATE TABLE IF NOT EXISTS prices(id TEXT UNIQUE PRIMARY KEY, usd TEXT, usdFoil TEXT, eur TEXT, eurFoil TEXT, FOREIGN KEY(id) REFERENCES card_info(id));';
+  static const createPurchaseUrisTableIfNotExists =
+      'CREATE TABLE IF NOT EXISTS purchase_uris(id TEXT UNIQUE PRIMARY KEY, tcgplayer TEXT, cardmarket TEXT, FOREIGN KEY(id) REFERENCES card_info(id));';
+  static const cardDatabaseTableFileName = 'cardDatabase.db';
 }
